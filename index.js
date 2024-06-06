78ea62d6691d2d0d418c3d1796a43c96add92832
Kejender <perttu.makkonen@gmail.com>
2024-06-06 18:09:00 +0300

    updates

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const express = require('express')
 const cors = require('cors')
-
 const app = express()
 app.use(cors())
+
+// uncomment for deployment
 app.use(express.static('dist'))
 
-
-//https://osdr.nasa.gov/geode-py/ws/api/hardware
-
+// test function, not needed
 function fetchHw(hwlinks) {
-
     hwlinks.forEach(element => {
         console.log(element.hardware)
-        //hwService.getHW(element.hardware)
     });
-
 }
 
-//let hwlinks
+// Getting the hardware list, which is a list of links to descriptions of
+// each hardware item. Each link is then fetched and the data is pushed to
+// hwlist array. The array is served to front end.
 
 let hwlist = []
 
@@ -37,60 +35,22 @@ fetch("https://osdr.nasa.gov/geode-py/ws/api/hardware")
                 })
         });
         //console.log(hwlist.length)
-    
     })
     .then(console.log("HWLIST"+hwlist))
     .catch((err)=>{
         console.log("error occured", err)
 })
 
-
-let hwdata
-
-fetch("https://osdr.nasa.gov/geode-py/ws/api/hardware/133")
-    .then((res)=> res.json())
-    .then((data)=>{
-        //console.log(data)
-        hwdata = data
-    })
-    .catch((err)=>{
-        console.log("error occured", err)
-})
-
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
+// sends the number of hw items
 app.get('/api/hw', (request, response) => {
-    response.send('<h1>Hardware '+hwlist.length+'</h1>')
+    response.send('<h1>Hardware items '+hwlist.length+'</h1>')
 })
 
+// sends the whole list of hardware items to front end
 app.get('/api/hwinfo', (request, response) => {
-
     console.log(typeof(hwlist))
-
     response.send(hwlist)
-    
-
-    
-    /*async function fetchHw() 
-    
-    
-    {
-        const response = await fetch("https://osdr.nasa.gov/geode-py/ws/api/hardware/133");
-        const hardware = await response.json();
-        console.log(hardware)
-        response.send(response.json())
-    }*/
-    //fetchHw();
 })
-
-/*async function fetchHw(hw) {
-  const response = await fetch(hw, {mode:'no-cors'});
-  const hardware = await response.json();
-  console.log(hardware)
-}*/
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
